b26889e3d857276e5cbf7973aba8702a0fb19be9
Matt Mitchell <mattmitchell.sail@yahoo.co.uk>
2026-04-23 21:05:31 +0100

    vercel analytics added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'  // 👈 ADD THIS
 import App from './App.jsx'
 import './styles/variables.css'
 import './styles/global.css'
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics /> {/* 👈 ADD THIS */}
     </HelmetProvider>
   </React.StrictMode>,
 )
